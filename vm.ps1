d03ea35e6637516738a0b79fdfb7eb9f034e40da
ehbLuca <luca.raymaekers@student.ehb.be>
2024-12-12 01:05:08 +0100

    Added: Check if ISO is already downloaded

--- vm.ps1
+++ vm.ps1
@@ -5,14 +5,33 @@
 [string]$SwitchName = "DebSquidSWitch"
 [int64]$MemoryStartupBytes = 2GB
 [int64]$VHDSize = 20GB
-# this should be provided as an argument or from an external known location.
-# mabye automatically download the iso and specify the path like that.
 [string]$ISOPath = "$VMPath\debian.iso"
+
+[string]$DebianURL = "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd"
+# Path to ISO file from $DebianURL
+[string]$ISODownloadFile = "debian-12.8.0-amd64-netinst.iso" 
+# Path to checksum file from $DebianURL
+[string]$ChecksumDownloadFile = "SHA512SUMS"
 
 New-Item -ItemType Directory -Path $VMPath -Force
 
-# The following command must be run as an administrator
+Write-Host "Downloading checksum."
+Invoke-WebRequest -Uri "$DebianURL/$ChecksumDownloadFile" -Outfile $ISOSumPath
 
+[string]$Line = Get-Content ./sha512sums | Select-String -Pattern $ISODownloadFile
+[string]$Checksum = $Line.Split(' ')[0].ToUpper()
+
+# If checksum matches with existing file we can skip the download
+if (
+    (Test-Path $ISOPath) -and
+    ($Checksum -eq (Get-FileHash $ISOPath -Algorithm SHA512).Hash)
+) {
+    Write-Host "Iso already downloaded."
+} else {
+    Invoke-Webrequest -Uri "$DebianURL/$ISODownloadFile" -Outfile $ISOPath -UseBasicParsing
+}
+
+# The following commands must be run with administrator privileges
 New-VHD -Path $VHDPath -SizeBytes $VHDSize -Dynamic
 New-VMSwitch -Name $SwitchName -NetAdapterName "Ethernet" -SwitchType External
 
